3246beebf59a8e1794b1278a5dda16c67fe20881
Isaac Lenton <isaac@isuniversal.com>
2018-03-05 17:46:26 +1000

    Documentation and move warning

--- ott-toolbox/isodd.m
+++ ott-toolbox/isodd.m
@@ -1,11 +1,12 @@
-function x = isodd(value)
-% Determines if an integer is odd
-% Input: isodd(integer)
-% Output: '1' if odd and '0' if even
+function x = isodd(input)
+% ISODD determines if an integer is odd
+% Outputs a matrix the same size as input with
+% 1 for odd and 0 for even entries.
 %
 % Warning: Plays up if the the integer is of the order 10^16
 %
 % PACKAGE_INFO
 
-x = mod(value,2);
+warning('this function will move to ott.utils.isodd');
 
+x = mod(input,2);
