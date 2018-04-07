7c20823269e8a9c93c81142eeaa734e9a37c0f8e
Isaac Lenton <isaac@isuniversal.com>
2018-04-07 12:45:13 +1000

    Removing beam_offset since it has a reasonable default

--- examples/example_gaussian.m
+++ examples/example_gaussian.m
@@ -55,11 +55,7 @@ w0 = lg_mode_w0( [ 0 0 ], beam_angle );
 % y-polarised, and [ 1 -i ] and [ 1 i ] are circularly polarised.
 polarisation = [ 1 i ];
 
-% Location of the focal point relative to the particle. These are the
-% [ x y z ] coordinates.
-beam_offset = [ 0 0 0];
-
-[n,m,a0,b0] = bsc_pointmatch_farfield(Nmax,1,[ 0 0 w0 1 polarisation 90 beam_offset ]);
+[n,m,a0,b0] = bsc_pointmatch_farfield(Nmax,1,[ 0 0 w0 1 polarisation 90 ]);
 [a,b,n,m] = make_beam_vector(a0,b0,n,m);
 
 %% Insert tmatrix here %%
