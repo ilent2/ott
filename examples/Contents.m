d9911589ee42e174ca653078dcec1b2874c641cf
Isaac Lenton <isaac@isuniversal.com>
2020-03-31 10:59:35 +1000

    Added contents files to examples and livescripts

---
+++ examples/Contents.m
@@ -0,0 +1,26 @@
+% OTT/EXAMPLES example scripts demonstrating different features of OTT
+%
+% If starting with the toolbox, take a look at the documentation
+% and the ``example_sphere.m`` and ``example_dynamics.m`` scripts.
+% There are also live scripts in the ``liveScripts`` directory
+% and a graphical user interface in the ``ott.ui`` package, see
+% documentation for more details.
+%
+% Files
+%   axial_equilibrium   - Calculate trapping position along z-axis
+%   beam_visualisation  - Generate near- and far-field beam visualisations
+%   example_dynamics    - Simulate particle moving in a Gaussian beam
+%   example_sphere      - Calculate forces on a spherical particle
+%   layered_sphere      - Calculate forces on a layered sphere T-matrix
+%   load_stl_shape      - Calculate a T-matrix from a STL geometry file
+%   multiple_beams      - Add multiple beams together
+%   slm_multi_focus     - Simulate a multifocus beam generated with an SLM
+%   slm_to_focalplane   - Simulate focussing of an SLM pattern by a microscope
+%   trapping_landscape  - Generate a trapping landscape figure
+%
+% Sub-folders
+%   liveScripts        - A collection of live script examples
+%
+% This file is part of the optical tweezers toolbox.
+% See LICENSE.md for information about using/distributing this file.
+
