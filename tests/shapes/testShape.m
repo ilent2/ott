36116081b8dbd3bfd31dcfb252948dcc5b1a4e64
Isaac Lenton <isaac@isuniversal.com>
2020-04-17 14:30:55 +1000

    Bugfix: input arguments of insideXyz incorrectly handled

--- tests/shapes/testShape.m
+++ tests/shapes/testShape.m
@@ -22,3 +22,27 @@ function testSimple(testCase)
       'Incorrect sphere radius');
 
 end
+
+function testInsideXyz(testCase)
+
+  radius = 1.0;
+  shape = ott.shapes.Shape.simple('sphere', radius);
+  
+  % Choose three points inside the shape and one outside
+  b = [true, true, true, false].';
+  x = [0.5*radius.*rand(1, 3), 4.0];
+  y = [0.5*radius.*rand(1, 3), 4.0];
+  z = [0.5*radius.*rand(1, 3), 4.0];
+  
+  xyz = [x(:), y(:), z(:)].';
+  
+  testCase.verifyEqual(shape.insideXyz(x, y, z), b, ...
+    'insideXyz with 3 arguments failed');
+  testCase.verifyEqual(shape.insideXyz(xyz), b, ...
+    'insideXyz with 1 argument failed');
+  
+  testCase.verifyEqual(shape.insideXyz(x, y, z, 'origin', 'world'), b, ...
+    'insideXyz with 3 arguments failed and optional arg');
+  testCase.verifyEqual(shape.insideXyz(xyz, 'origin', 'world'), b, ...
+    'insideXyz with 1 argument failed and optional arg');
+end
--- tests/shapes/testStlLoader.m
+++ tests/shapes/testStlLoader.m
@@ -20,3 +20,27 @@ function testLoad(testCase)
     'Incorrect number of faces');
 
 end
+
+function testInsideXyz(testCase)
+
+  shape = ott.shapes.StlLoader('cube.stl');
+  radius = shape.maxRadius;
+  
+  % Choose three points inside the shape and one outside
+  b = [true, true, true, false].';
+  x = [0.5*radius.*rand(1, 3), 4.0];
+  y = [0.5*radius.*rand(1, 3), 4.0];
+  z = [0.5*radius.*rand(1, 3), 4.0];
+  
+  xyz = [x(:), y(:), z(:)].';
+  
+  testCase.verifyEqual(shape.insideXyz(x, y, z), b, ...
+    'insideXyz with 3 arguments failed');
+  testCase.verifyEqual(shape.insideXyz(xyz), b, ...
+    'insideXyz with 1 argument failed');
+  
+  testCase.verifyEqual(shape.insideXyz(x, y, z, 'origin', 'world'), b, ...
+    'insideXyz with 3 arguments failed and optional arg');
+  testCase.verifyEqual(shape.insideXyz(xyz, 'origin', 'world'), b, ...
+    'insideXyz with 1 argument failed and optional arg');
+end
