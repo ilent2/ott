cd26aaaa7057c462e372ff45efccaeeaf6684303
Isaac Lenton <isaac@isuniversal.com>
2020-05-06 04:31:53 +1000

    Array cat for beams

--- +ott/+beam/+abstract/Beam.m
+++ +ott/+beam/+abstract/Beam.m
@@ -5,6 +5,9 @@ classdef (Abstract) Beam < ott.beam.Properties
 % Classes that inherit from this class include a description of the
 % beam but do not necessarily include methods to calculate the fields.
 %
+% Methods
+%   - cat               -- Concatenate two beams (creates a Array)
+%
 % Abstract methods
 %   - getBeamPower      -- get method called by dependent property power
 
@@ -21,6 +24,42 @@ classdef (Abstract) Beam < ott.beam.Properties
       % For optional parameters, see :class:`Properties`.
 
       beam = beam@ott.beam.Properties(varargin{:});
+    end
+
+    function beam = horzcat(varargin)
+      % Concatenate beam objects.
+      %
+      % Usage
+      %   beam = [beam1, beam2, ...]
+      %   Defers to cat(2, ...).
+
+      beam = cat(2, varargin{:});
+    end
+
+    function beam = vertcat(varargin)
+      % Concatenate beam objects.
+      %
+      % Usage
+      %   beam = [beam1; beam2; ...]
+      %   Defers to cat(1, ...).
+
+      beam = cat(1, varargin{:});
+    end
+
+    function beam = cat(dim, varargin)
+      % Concatenate two beam objects.
+      %
+      % By default, this creates a new beam Array.
+      % If you class has a different behaviour, either overload this
+      % function or use the :class:`ott.beam.utils.ArrayType` class.
+      %
+      % Usage
+      %   beam = cat(dim, beam1, beam2, beam3, ...)
+
+      sz = ones(1, dim+1);
+      sz(dim) = numel(varargin);
+      beam = ott.beam.utils.ArrayType.AutoArray('array', ...
+          sz, varargin{:});
     end
   end
 end
