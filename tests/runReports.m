4d96672ae4303b51c30f5b3c6e85efe6ab032336
Isaac Lenton <isaac@isuniversal.com>
2020-11-06 15:36:49 +1000

    Added a script to run contents/analyser reports

---
+++ tests/runReports.m
@@ -0,0 +1,20 @@
+% This file runs the code analyser reports for each toolbox package
+
+reportlist = {};
+reportlist{end+1} = '../+ott/+beam';
+reportlist{end+1} = '../+ott/+bsc';
+reportlist{end+1} = '../+ott/+particle';
+reportlist{end+1} = '../+ott/+tmatrix';
+reportlist{end+1} = '../+ott/+shape';
+reportlist{end+1} = '../+ott/+drag';
+reportlist{end+1} = '../+ott/+tools';
+reportlist{end+1} = '../+ott/+utils';
+reportlist{end+1} = '../examples';
+
+for ii = 1:numel(reportlist)
+  web('', '-new');
+  mlintrpt(reportlist{ii},'dir');
+  
+  web('', '-new');
+  contentsrpt(reportlist{ii});
+end
