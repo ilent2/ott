cd190408860d87cdd1571bb45767f3a3718718a4
Isaac Lenton <isaac@isuniversal.com>
2020-11-08 14:27:28 +1000

    Implemented FindTraps1d

--- +ott/+tools/FindTraps1d.m
+++ +ott/+tools/FindTraps1d.m
@@ -36,7 +36,7 @@ classdef FindTraps1d
     position(1,1) double        % Trap position
     stiffness(1,1) double       % Trap stiffness at equilibrium
     depth(1,1) double           % Optical trap depth
-    range(2,1) double           % Range of optical trap
+    range(1,2) double           % Range of optical trap
     minforce(1,1) double        % Minimum force in trap
     maxforce(1,1) double        % Maximum force in trap
     minposition(1,1) double     % Position of minimum force
@@ -49,7 +49,11 @@ classdef FindTraps1d
 
   methods (Static)
     function traps = FromArray(position, force, varargin)
-      % Find traps in an array of position/force values
+      % Find traps in an array of position/force values.
+      %
+      % It may be useful to pre-filter the arrays, such as applying a
+      % low pass filter to the forces to remove high frequency noise or
+      % rounding small values to zero.
       %
       % Usage
       %   traps = ott.tools.FindTraps1d.FromArray(position, force, ...)
@@ -79,7 +83,141 @@ classdef FindTraps1d
       position = position(:);
       force = force(:);
 
-      % TODO
+      % Find rough equilibrium positions
+
+      % Find first equilibrium position
+      if force(1) > 0
+        last = find(force < 0, 1);
+      elseif force(1) == 0
+        last = find(force ~= 0, 1);
+      else
+        last = find(force > 0, 1);
+      end
+
+      % Find reaming equilibriums
+      eqs = [];
+      while ~isempty(last)
+        eqs(end+1) = last; %#ok<AGROW>
+
+        if force(last) > 0
+          last = find(force(last:end) < 0, 1) + last - 1;
+        else
+          last = find(force(last:end) > 0, 1) + last - 1;
+        end
+      end
+
+      % Find more precise equilibrium positions and trap stiffness
+      % This is based on ott.find_equilibrium
+
+      peqs = zeros(1, length(eqs));
+      pstiff = zeros(1, length(eqs));
+
+      for ii = 1:length(eqs)
+
+        % For extended zeros we might have several points equal to zero.
+        % For the polyfit, we want to use a point at each end of the equilibrium
+        % and all points in between.
+        first = find(force(1:eqs(ii)-1)*sign(force(eqs(ii))) < 0, 1, 'last');
+        if isempty(first)
+          first = 1;
+        end
+
+        % Equilibrium is between eqs(ii)-1 and eqs(ii)
+        eqguess = (position(eqs(ii)) + position(first))/2;
+
+        % Fit polynomial to points aroung equilibrium
+        eqrange = max([first-1,1]):min([eqs(ii)+1,length(position)]);
+        z = position(eqrange);
+
+        % Scale position and force before polyfit
+        zmin = min(z);
+        zmax = max(z);
+        z = 2 * (z - zmin) / (zmax - zmin) - 1;
+        zzero = 2 * (eqguess - zmin) / (zmax - zmin) - 1;
+
+        % Find equilibrium: fit local points to 3rd order polynomail
+        % Requires small distance between positions
+        if length(z) < 4
+          pz=polyfit(z, force(eqrange), 2);
+          dpz=[2*pz(1),1*pz(2)];
+        else
+          pz=polyfit(z, force(eqrange), 3);
+          dpz=[3*pz(1),2*pz(2),1*pz(3)];
+        end
+        root_z=roots(pz);
+
+        % Ignore non-real roots
+        real_z=root_z(imag(root_z)==0);
+        if numel(real_z) == 0
+          error('No real roots');
+        end
+
+        % Keep only one root closest to position(eqs(ii))
+        zeqs_idx = abs(real_z-zzero) == min(abs(real_z-zzero));
+        real_z = real_z(zeqs_idx);
+        real_z = real_z(1);
+
+        % Get the equilibrium
+        peqs(ii) = real_z;
+
+        % Calculate stiffness (using derivative of 3rd order polynomial)
+        pstiff(ii) = polyval(dpz, peqs(ii));
+
+        % Inverse scaling of position and force after fitting
+        peqs(ii) = (peqs(ii) + 1)/2*(zmax - zmin) + zmin;
+        pstiff(ii) = pstiff(ii)*2/(zmax - zmin);
+
+      end
+
+      % Calculate other properties needed for traps
+
+      traps = ott.tools.FindTraps1d.empty(1, 0);
+
+      for ii = 1:length(eqs)
+
+        % Calculate trap depth (part 1)
+        frange = [1, length(force)];
+        if ii ~= 1
+          frange(1) = eqs(ii-1);
+        end
+        if ii ~= length(eqs)
+          frange(2) = eqs(ii+1)-1;
+        end
+        [fn, fnidx] = min(force(frange(1):frange(2)));
+        [fx, fxidx] = max(force(frange(1):frange(2)));
+
+        idx = length(traps) + 1;
+
+        % Store stiffness and equilibrium
+        traps(idx).position = peqs(ii);
+        traps(idx).stiffness = pstiff(ii);
+
+        % Calculate trap range
+        traps(idx).range = [-Inf, Inf];
+        if ii ~= 1
+          traps(idx).range(1) = peqs(ii-1);
+        end
+        if ii ~= length(eqs)
+          traps(idx).range(2) = peqs(ii+1);
+        end
+
+        % Calculate trap depth (part 2)
+        if fnidx < fxidx
+          [fx, fn] = deal(fn, fx);
+          [fxidx, fnidx] = deal(fnidx, fxidx);
+        end
+        traps(idx).minforce = fx;
+        traps(idx).maxforce = fn;
+        traps(idx).minposition = position(frange(1)+fxidx-1);
+        traps(idx).maxposition = position(frange(1)+fnidx-1);
+        traps(idx).depth = min(abs(...
+            [traps(idx).maxforce, traps(idx).minforce]));
+      end
+
+      % Remove unstable traps
+      if ~p.Results.keep_unstable
+        traps = traps.filterUnstable();
+      end
     end
 
     function traps = Bisection(position, force, varargin)
@@ -106,17 +244,76 @@ classdef FindTraps1d
       %
       %   - StepTol (numeric) -- Minimum step size tolerance for stopping.
       %     Default: ``1e-10`` (should work for 1um wavelength
-      %     beams with nm particles in SI units).
+      %     beams with nm particles in SI units).  This is also used
+      %     for calculating the ``stiffness`` property.
 
-      % TODO
+      p = inputParser;
+      p.addParameter('ForceTol', 1e-10);
+      p.addParameter('StepTol', 1e-13);
+      p.parse(varargin{:});
+
+      stepTol = p.Results.StepTol;
+      forceTol = p.Results.ForceTol;
+
+      % Check inputs
+      assert(isa(force, 'function_handle'), ...
+          'Force must be a function handle');
+      assert(isnumeric(position) && numel(position) == 2, ...
+          'Position must be 2 element numeric vector');
+      assert(isnumeric(stepTol) && isscalar(stepTol) && stepTol > 0, ...
+          'StepTol must be positive numeric scalar');
+      assert(isnumeric(forceTol) && isscalar(forceTol) && forceTol > 0, ...
+          'ForceTol must be positive numeric scalar');
+
+      % Evaluate initial forces
+      f1 = force(position(1));
+      f2 = force(position(2));
+
+      % Check forces
+      assert(isnumeric(f1) && isscalar(f1), ...
+          'Value returned by force function should be numeric scalar');
+
+      % Check sign on initial points
+      if sign(f1) == sign(f2)
+        warning('ott:tools:FindTraps1d:Bisection:bounds_sign', ...
+            'Initial forces have same sign, may be no stable equilibrium');
+      end
+
+      while diff(position) > stepTol && ~any(abs([f1, f2]) < forceTol)
+
+        % Calculate new point
+        midp = mean(position);
+        midf = force(midp);
+
+        if sign(midf) == sign(f1)
+          f1 = midf;
+          position(1) = midp;
+        else
+          f2 = midf;
+          position(2) = midp;
+        end
+      end
+
+      % Find best trap
+      [f1, I] = min(abs([f1, f2]));
+      traps = ott.tools.FindTraps1d();
+      traps.position = position(I);
+      traps.stiffness = (f1 - force(position(I) - stepTol))./stepTol;
+      traps.depth = nan;
+      traps.range = [nan, nan];
+      traps.minforce = nan;
+      traps.maxforce = nan;
+      traps.minposition = nan;
+      traps.maxposition = nan;
     end
 
     function traps = Newton(position, force, varargin)
       % Find trap starting with a initial guess at the location.
       %
       % Uses Newtons method and moves the particle in the direction
-      % given by the optical force at each step.  The trap should be
-      % a stable equilibrium.
+      % given by the optical force at each step.  Searches for a stable
+      % trap, if you want to find an unstable trap, modify the force
+      % function to give negative forces.
       %
       % Usage
       %   traps = ott.tools.FindTraps1d.Newton(position, force, ...)
@@ -138,21 +335,90 @@ classdef FindTraps1d
       %   - ForceTol (numeric) -- Absolute tolerance for stopping.
       %     Default: ``1e-13`` (should work for 1mW beams in SI units).
 
-      % TODO
+      p = inputParser;
+      p.addParameter('maxIterations', 100);
+      p.addParameter('smallStep', 1e-10);
+      p.addParameter('ForceTol', 1e-13);
+      p.parse(varargin{:});
+
+      maxIter = p.Results.maxIterations;
+      smallStep = p.Results.smallStep;
+      forceTol = p.Results.ForceTol;
+
+      % Check inputs
+      assert(isnumeric(position) && isscalar(position), ...
+          'position must be numeric scalar');
+      assert(isa(force, 'function_handle'), ...
+          'force must be a function handle');
+      assert(isnumeric(maxIter) && isscalar(maxIter) && maxIter > 0, ...
+          'maxIter must be positive numeric scalar');
+      assert(isnumeric(smallStep) && isscalar(smallStep), ...
+          'smallStep must be numeric scalar');
+      assert(isnumeric(forceTol) && isscalar(forceTol) && forceTol > 0, ...
+          'forceTol must be positive numeric scalar');
+
+      % Get initial force
+      fv = force(position);
+      dvf = (force(position + smallStep) - fv) ./ smallStep;
+
+      % Check force
+      assert(isnumeric(fv) && isscalar(fv), ...
+          'force must return a numeric scalar');
+
+      % Newton's iteration
+      for ii = 1:maxIter
+        
+        % Calculate step
+        position = position - fv ./ dvf;
+        
+        % Calculate new values
+        fv = force(position);
+        dvf = (force(position + smallStep) - fv) ./ smallStep;
+
+        % Check for stopping condition
+        if abs(fv) <= forceTol
+          break;
+        end
+      end
+
+      % Final check of force value
+      if abs(fv) > forceTol
+        warning('ott:tools:FindTraps1d:Newton:final_force_check', ...
+            'Final force not within force tolerance');
+      end
+
+      % Setup output
+      traps = ott.tools.FindTraps1d();
+      traps.position = position;
+      traps.stiffness = dvf;
+      traps.depth = nan;
+      traps.range = [nan, nan];
+      traps.minforce = nan;
+      traps.maxforce = nan;
+      traps.minposition = nan;
+      traps.maxposition = nan;
     end
   end
 
   methods
-    function varargout = plot(traps, varargin)
-      % Generate plot of the traps in the array
+    function h = plot(traps, varargin)
+      % Generate plot of the traps in the array.
+      %
+      % Squares mark equilibriums.  Solid lines (with crosses) mark
+      % stable traps.  Dashed lines (with circles) mark unstable traps.
       %
       % Usage
-      %   [...] = traps.plot(...)
+      %   h = traps.plot(...)
       %
-      % Additional parameters/outputs passed to/from ``plot``.
+      % Returns
+      %   - h -- An array of plot handles.  The first handle is the
+      %     eqilibrium markers.  Remaining handles are trap lines.
+
+      p = inputParser;
+      p.parse(varargin{:});
 
       % Plot trap locations
-      plot([traps.position], zeros(size(traps)), 'x');
+      h = plot([traps.position], zeros(size(traps)), 's');
 
       % Setup hold
       oldHold = ishold();
@@ -160,12 +426,12 @@ classdef FindTraps1d
 
       % Plot trap ranges
       lineType = [traps.globalStiffness] < 0;
-      minForces = [traps.minforce];
-      maxForces = [traps.maxforce];
-      plot([minForces(lineType), maxForces(lineType)], ...
-          zeros(sum(lineType), 2), 'kx-');
-      plot([minForces(~lineType), maxForces(~lineType)], ...
-          zeros(sum(~lineType), 2), 'ro-');
+      minPositions = [traps.minposition];
+      maxPositions = [traps.maxposition];
+      h = [h; plot([minPositions(lineType); maxPositions(lineType)], ...
+          zeros(2, sum(lineType)), 'kx-')];
+      h = [h; plot([minPositions(~lineType); maxPositions(~lineType)], ...
+          zeros(2, sum(~lineType)), 'ro--')];
 
       % Return hold to initial state
       if ~oldHold
@@ -187,6 +453,9 @@ classdef FindTraps1d
       %   - stats (FindTraps1d) -- Meta traps (with no position or
       %     stiffness) describing each group.
 
+      p = inputParser;
+      p.parse(varargin{:});
+
       % Identify trap groups
       %
       % We do two passes to identify the maximum and minimum trapping
@@ -194,21 +463,21 @@ classdef FindTraps1d
 
       idxer = 0;
       group_idx_max = zeros(1, length(traps));
-      maxforce = 0;
+      omaxforce = 0;
       for ii = 1:length(traps)
-        if traps(ii).minmax_force(1) > maxforce
-          maxforce = traps(ii).minmax_force(1);
+        if traps(ii).minforce > omaxforce
+          omaxforce = traps(ii).minforce;
           idxer = idxer + 1;
         end
         group_idx_max(ii) = idxer;
       end
 
       idxer = 0;
-      minforce = 0;
+      ominforce = 0;
       group_idx_min = zeros(1, length(traps));
       for ii = length(traps):-1:1
-        if traps(ii).minmax_force(2) < minforce
-          minforce = traps(ii).minmax_force(2);
+        if traps(ii).maxforce < ominforce
+          ominforce = traps(ii).maxforce;
           idxer = idxer + 1;
         end
         group_idx_min(ii) = idxer;
@@ -219,7 +488,7 @@ classdef FindTraps1d
       % Create list of trap groups
       unique_group_idx = unique(group_idx);
       grouped = cell(1, numel(unique_group_idx));
-      stats(numel(grouped)) = ott.tools.FieldTraps1d;
+      stats(numel(grouped)) = ott.tools.FindTraps1d;
       for ii = 1:numel(unique_group_idx)
         grouped{ii} = traps(group_idx == unique_group_idx(ii));
 
@@ -241,8 +510,31 @@ classdef FindTraps1d
       %
       % Usage
       %   traps = traps.filterShallow(...)
+      %
+      % Optional named parameters
+      %   - AbsTol (numeric | []) -- Absolute tolerance for removing
+      %     shallow traps.  Default: ``[]``.
+      %
+      %   - RelTol (numeic | []) -- Relative tolerance for removing shallow
+      %     traps.  Relative to maximum trap depth.  Default: ``1e-2``.
 
-      % TODO
+      p = inputParser;
+      p.addParameter('AbsTol', []);
+      p.addParameter('RelTol', []);
+      p.parse(varargin{:});
+
+      notShallow = true(size(traps));
+
+      if ~isempty(p.Results.AbsTol)
+        notShallow = notShallow & [traps.depth] >= p.Results.AbsTol;
+      end
+
+      if ~isempty(p.Results.RelTol)
+        maxDepth = max([traps.depth]);
+        notShallow = notShallow & [traps.depth] >= p.Results.RelTol*maxDepth;
+      end
+
+      traps = traps(notShallow);
     end
 
     function traps = filterUnstable(traps, varargin)
@@ -251,7 +543,11 @@ classdef FindTraps1d
       % Usage
       %   traps = traps.fitlerUnstable(...)
 
-      % TODO
+      p = inputParser;
+      p.parse(varargin{:});
+
+      stable = [traps.stiffness] < 0 | [traps.globalStiffness] < 0;
+      traps = traps(stable);
     end
   end
 
--- tests/tools/testFindTraps1d.m
+++ tests/tools/testFindTraps1d.m
@@ -19,70 +19,86 @@ function testPlot(testCase)
 
   x = linspace(-1, 1);
   x(1) = []; x(end) = [];  % Drop zeros at ends
-  y = -2*sin(pi*x);
+  y = -2*sin(3*pi*x);
 
-  traps = ott.tools.FindTraps1d.FromArray(x, y);
+  traps = ott.tools.FindTraps1d.FromArray(x, y, 'keep_unstable', true);
 
   h1 = plot(x, y);
   hold on;
   h2 = traps.plot();
   hold off;
 
-  testCase.verifyThat(ishandle(h1), 'h1');
-  testCase.verifyThat(ishandle(h2), 'h2');
+  testCase.verifyTrue(all(ishandle(h1)), 'h1');
+  testCase.verifyTrue(all(ishandle(h2)), 'h2');
+
+end
+
+function testBisection(testCase)
+
+  x = [-0.5, 0.5];
+  y = @(x) -2*sin(pi*x);
+
+  traps = ott.tools.FindTraps1d.Bisection(x, y);
+  
+  testCase.verifyEqual(traps.position, 0, 'p');
+  testCase.verifyEqual(traps.stiffness, -2*pi, 's');
+
+end
+
+function testNewton(testCase)
+
+  x = 0.2;
+  y = @(x) -2*sin(pi*x);
+
+  traps = ott.tools.FindTraps1d.Newton(x, y);
+  
+  testCase.verifyWarningFree(@() ott.tools.FindTraps1d.Newton(x, y));
+  
+  testCase.verifyEqual(traps.position, 0, 'AbsTol', 1e-15, 'p');
+  testCase.verifyEqual(traps.stiffness, -2*pi, 's');
 
 end
 
 function testSingleTrapGroup(testCase)
 
-  % TODO:Rewrite
-
-  import matlab.unittest.constraints.IsEqualTo;
-
   y = [0.1, 1, -1, -0.1];
   x = 1:numel(y);
 
-  traps = ott.find_traps(x, y, 'keep_unstable', true);
+  traps = ott.tools.FindTraps1d.FromArray(x, y, 'keep_unstable', true);
 
-  testCase.verifyThat(numel(traps), IsEqualTo(1), ...
+  testCase.verifySize(traps, [1, 1], ...
       'Wrong number of traps identified with keep_unstable=true');
 
-  traps = ott.find_traps(x, y, 'keep_unstable', false);
+  traps = ott.tools.FindTraps1d.FromArray(x, y, 'keep_unstable', false);
 
-  testCase.verifyThat(numel(traps), IsEqualTo(1), ...
+  testCase.verifySize(traps, [1, 1], ...
       'Wrong number of traps identified with keep_unstable=false');
 
-  traps = ott.find_traps(x, y, 'keep_unstable', false, 'group_stable', true);
+  [~, stats] = traps.groupStable();
 
-  testCase.verifyThat(numel(traps), IsEqualTo(1), ...
+  testCase.verifySize(numel(stats), [1,1], ...
       'Wrong number of traps identified with group_stable=true');
 
 end
 
 function testDoubleTrapGroup(testCase)
 
-  % TODO:Rewrite
-
-  import matlab.unittest.constraints.IsEqualTo;
-  import matlab.unittest.constraints.AbsoluteTolerance;
-  tol = 1e-2;
-
   y = [0.1, 1, -1, 0.5, -0.5, 0.3, -2, -1];
   x = 1:numel(y);
 
-  traps = ott.find_traps(x, y, 'keep_unstable', true);
+  traps = ott.tools.FindTraps1d.FromArray(x, y, 'keep_unstable', true);
 
-  testCase.verifyThat(numel(traps), IsEqualTo(5), ...
+  testCase.verifySize(traps, [1, 5], ...
       'Wrong number of traps identified with keep_unstable=true');
 
-  traps = ott.find_traps(x, y, 'keep_unstable', false);
+  traps = ott.tools.FindTraps1d.FromArray(x, y, 'keep_unstable', false);
 
-  testCase.verifyThat(numel(traps), IsEqualTo(3), ...
+  testCase.verifySize(traps, [1, 3], ...
       'Wrong number of traps identified with keep_unstable=false');
 
-  traps = ott.find_traps(x, y, 'keep_unstable', false, 'group_stable', true);
+  [~, stats] = traps.groupStable();
 
-  testCase.verifyThat(numel(traps), IsEqualTo(1), ...
+  testCase.verifySize(numel(stats), [1,1], ...
       'Wrong number of traps identified with group_stable=true');
 
 end
