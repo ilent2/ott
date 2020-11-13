69998986aab683240cfe8ab7c403fbe458a25645
Isaac Lenton <isaac@isuniversal.com>
2020-11-14 02:25:47 +1000

    Added showCoverage flag to test runner

--- tests/run.m
+++ tests/run.m
@@ -6,6 +6,8 @@ import matlab.unittest.TestSuite
 import matlab.unittest.TestRunner
 import matlab.unittest.plugins.CodeCoveragePlugin
 
+showCoverage = false;
+
 % Array of sub-packages to test
 subpackages = {};
 subpackages{end+1} = 'beam';
@@ -22,15 +24,17 @@ subpackages{end+1} = 'utils';
 
 % Array of sub-packages to generate coverage reports for
 coverage = {};
-coverage{end+1} = 'ott.beam';
-coverage{end+1} = 'ott.bsc';
-coverage{end+1} = 'ott.drag';
-coverage{end+1} = 'ott.particle';
-coverage{end+1} = 'ott.shape';
-coverage{end+1} = 'ott.tmatrix';
-coverage{end+1} = 'ott.tools';
-%coverage{end+1} = 'ott.ui';
-coverage{end+1} = 'ott.utils';
+if showCoverage
+  coverage{end+1} = 'ott.beam';
+  coverage{end+1} = 'ott.bsc';
+  coverage{end+1} = 'ott.drag';
+  coverage{end+1} = 'ott.particle';
+  coverage{end+1} = 'ott.shape';
+  coverage{end+1} = 'ott.tmatrix';
+  coverage{end+1} = 'ott.tools';
+  %coverage{end+1} = 'ott.ui';
+  coverage{end+1} = 'ott.utils';
+end
 
 % Generate coverage report runner for packages
 runner = TestRunner.withTextOutput;
@@ -39,9 +43,11 @@ if ~isempty(coverage)
       coverage, 'IncludingSubpackages', true));
 end
 
-% Add coverage report for examples directory
-runner.addPlugin(CodeCoveragePlugin.forFolder('../examples', ...
-    'IncludingSubfolders', true));
+if showCoverage
+  % Add coverage report for examples directory
+  runner.addPlugin(CodeCoveragePlugin.forFolder('../examples', ...
+      'IncludingSubfolders', true));
+end
 
 % Build test suite
 suites = {};
