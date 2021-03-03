7930eb7fbb8a867df7af29d9c999a31baa13aad2
Isaac Lenton <isaac@isuniversal.com>
2021-03-03 16:25:46 +0100

    Create Contents.m

---
+++ docs/Contents.m
@@ -0,0 +1,31 @@
+% ott/docs/Contents.m
+%
+% This directory contains the Sphinx documentation for the project.
+% The documentation is written using restructured text, it is somewhat
+% human readable, however for a better experience it is reccomended
+% to compile the documentation into PDF or HTML output.
+% Alternatively, refer to the online documentation at
+% https://ott.rtfd.org
+%
+% Contents:
+%   - .gitignore
+%   - Beam-Packages.rst
+%   - Bsc-Package.rst
+%   - Documentation-terms-of-use.rst
+%   - Drag-Package.rst
+%   - Example-Advanced.rst
+%   - Example-Beams.rst
+%   - Example-Force.rst
+%   - Example-Gui.rst
+%   - Example-Particles.rst
+%   - Examples.rst
+%   - Makefile
+%   - Particle-Package.rst
+%   - Reference.rst
+%   - Shape-Package.rst
+%   - Tmatrix-Package.rst
+%   - conf.py
+%   - documentation_toc.rst
+%   - index.rst
+%   - make.bat
+%   - requirements.txt
