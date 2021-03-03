898bb575cf2b6232d288ae396b2d44ff5d11843a
Isaac Lenton <isaac@isuniversal.com>
2021-03-03 16:07:40 +0100

    Create Contents.m

---
+++ tests/Contents.m
@@ -0,0 +1,21 @@
+% ott/tests/Contents.m
+%
+% This directory contains unit tests for verifying that
+% the toolbox features behave as expected.  With the initial
+% release of OTTv2 the goal was to achieve thougher coverage.
+%
+% Sub-directories
+%   - beam
+%   - bsc
+%   - drag
+%   - dynamics
+%   - examples
+%   - particle
+%   - shape
+%   - tmatrix
+%   - tools
+%   - utils
+%
+% Scripts
+%   - run.m          -- Run all tests
+%   - runReports.m   -- Run reports (such as code coverage)
