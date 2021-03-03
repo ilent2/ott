34e020a0ca28110b5ddd3f80ccf1b78af99b3285
Isaac Lenton <isaac@isuniversal.com>
2021-03-03 16:03:26 +0100

    Update and rename README.md to Contents.m

---
+++ thirdparty/Contents.m
@@ -0,0 +1,13 @@
+% ott/thirdparty/Contents.m
+%
+% This directory contains licenses for third party code that is
+% used by part of this toolbox.  Functions that contain third
+% party code have a reference to the license file in this
+% directory.
+%
+%   - Contents.m    -- This file
+%   - SMARTIES.txt  -- SMARTIES license
+%   - auxInitLegendreQuad_Winckel.txt
+%   - stl_EricJohnson.txt
+%   - stl_KrishnanSuresh.txt
+%   - stl_Sven.txt
--- thirdparty/README.md
+++
@@ -1,6 +0,0 @@
-
-This directory contains licenses for third party code that is
-used by part of this toolbox.  Functions that contain third
-party code have a reference to the license file in this
-directory.
-
