35fd6d6bc07a811191168036786400e2036d3606
Isaac Lenton <isaac@isuniversal.com>
2021-03-23 13:23:52 +0100

    Added bsc package overview figure

--- examples/packageOverview/bscs.m
+++ examples/packageOverview/bscs.m
@@ -6,7 +6,8 @@
 % Add toolbox to path (uncomment this line if OTT is not already on the path)
 %addpath('../../');
 
-figure();
+hf = figure();
+hf.Position = [680 877 642 221];
 
 rows = 1;
 cols = 4;
@@ -14,26 +15,32 @@ idx = 1;
 
 %% Simple BSC beams
 
+subplot(rows, cols, idx); idx = idx + 1;
+
 bsc = ott.bsc.Bsc([1;0;0], zeros(3, 1));
 beam = ott.beam.BscBeam(bsc);
-beam.visFarfieldSphere('type', '3dpolar');
+beam.visFarfieldSphere('type', '3dpolar', ...
+    'field', 'E2', 'normalise', true);
 title('Bsc');
+set(gca(), 'XTick', [], 'YTick', []);
 
 %% Plane wave basis
 
 subplot(rows, cols, idx); idx = idx + 1;
 
 beam = ott.beam.PlaneWave();
-beam.visNearfield('axis', 'y', 'field', 'Re(Ex)');
+beam.visNearfield('axis', 'y', 'field', 'Re(Ex)', 'range', 1e-6*[1,1]);
 title('PlaneWave');
+set(gca(), 'XTick', [], 'YTick', []);
 
 %% Annular basis
 
 subplot(rows, cols, idx); idx = idx + 1;
 
 beam = ott.beam.Bessel('theta', pi/4);
-beam.visNearfield('axis', 'y', 'field', 'Re(Ex)');
+beam.visNearfield('axis', 'y', 'field', 'Re(Ex)', 'range', 1e-6*[1,1]);
 title('Annular');
+set(gca(), 'XTick', [], 'YTick', []);
 
 %% Pointmatched beams
 
@@ -42,3 +49,4 @@ subplot(rows, cols, idx); idx = idx + 1;
 beam = ott.beam.LaguerreGaussian('lmode', 1, 'pmode', 2);
 beam.visNearfield();
 title('Pointmatch');
+set(gca(), 'XTick', [], 'YTick', []);
