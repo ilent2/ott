9a3dd48d72aa33cdb5455e0691fff67a75d16667
Isaac Lenton <isaac@isuniversal.com>
2021-05-18 21:33:15 +0200

    Added DDA widgets

--- +ott/+ui/+tmatrix/Dda.m
+++ +ott/+ui/+tmatrix/Dda.m
@@ -14,18 +14,77 @@ classdef Dda < ott.ui.tmatrix.NewTmatrixBase
       'approximation.'];
     
     helpText = {ott.ui.tmatrix.Dda.aboutText, ...
-      ''};
+      '', ...
+      'Spacing : Distance between voxels.', ...
+      '', ...
+      'Low Memory : When true, uses the low memory DDA method.'};
     
     windowName = ott.ui.tmatrix.Dda.nameText;
   end
   
+  properties (Access=public)
+    SpacingSpinner      ott.ui.support.LabeledSpinner
+    LowMemoryCheckhbox  matlab.ui.control.CheckBox
+  end
+  
   methods (Access=protected)
+    
+    function setDefaultValues(app)
+      % Set default values of our widgets
+      
+      app.SpacingSpinner.Value = 1/20;
+      app.LowMemoryCheckhbox.Value = false;
+      
+      % Let base do the rest
+      setDefaultValues@ott.ui.tmatrix.NewTmatrixBase(app);
+    end
+    
     function data = generateData(app)
-      data = ott.tmatrix.Dda();  % TODO: parameters
+      % Generate T-matrix
+      
+      data = ott.tmatrix.Dda.FromShape(app.ShapeName.Variable, ...
+          'spacing', app.SpacingSpinner.Value, ...
+          'index_relative', app.RelativeIndexSpinner.Value, ...
+          'low_memory', app.LowMemoryCheckbox.Value);
     end
     
     function code = generateCode(app)
-      code = {}; % TODO
+      code = {};
+      
+      % Code for T-matrix generation
+      code{end+1} = ['tmatrix = ott.tmatrix.Dda.FromShape(' app.ShapeName.Value ', ...'];
+      code{end+1} = ['    ''spacing'', ' app.SpacingSpinner.Value ', ...'];
+      code{end+1} = ['    ''index_relative'', ' app.RelativeIndexSpinner.Value ', ...'];
+      code{end+1} = ['    ''low_memory'', ' app.LowMemoryCheckbox.Value ');'];
+    end
+    
+    function createMainComponents(app)
+      
+      % Call base for most things
+      createMainComponents@ott.ui.tmatrix.NewTmatrixBase(app);
+      
+      % Create grid
+      app.ExtraGrid.RowHeight = repmat({32}, 1, 3);
+      app.ExtraGrid.RowHeight{end} = '1x';
+      app.ExtraGrid.RowSpacing = 1;
+      
+      % Spacing spinner
+      app.SpacingSpinner = ott.ui.support.LabeledSpinner(app.ExtraGrid, ...
+          'label', 'Spacing');
+      app.SpacingSpinner.Layout.Row = 1;
+      app.SpacingSpinner.Layout.Column = 1;
+      app.SpacingSpinner.Step = 0.01;
+      app.SpacingSpinner.Limits = [0,Inf];
+      app.SpacingSpinner.LowerLimitInclusive = false;
+      app.SpacingSpinner.ValueChangedFcn = @(~,~) app.updateParametersCb();
+      
+      % Low memory checkbox
+      app.LowMemoryCheckhbox = uicheckbox(app.ExtraGrid);
+      app.LowMemoryCheckhbox.Text = 'Low Memory';
+      app.LowMemoryCheckhbox.Layout.Column = 1;
+      app.LowMemoryCheckhbox.Layout.Row = 2;
+      app.LowMemoryCheckhbox.ValueChangedFcn = @(~,~) app.updateParametersCb();
+      
     end
   end
   
